f756633c0369631f2de2132e4e8b1e9b175458e8
Batsh-dev <babatsh028@gmail.com>
2019-10-01 18:46:44 +0200

    app

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 console.log('How old are you? ');
-process.stdin.on('data', (number) => {
-    if(typeof number === "number"){
-        console.log("Is not a number");
-    }else if(number > 99){
+process.stdin.on('data', (nbr) => {
+    if(nbr > 0 && nbr <100){
+        nbr = 2019 - nbr;
+        console.log('You were born in ' + nbr);
+    }else if(nbr > 99){
         console.log("you are not so old");
-    }else if(number < 0){
+    }else if(nbr < 0){
         console.log("You can not have a negative or no age");
     }else{
-        number = 2019 - number;
-        console.log('You were born in' + number);
+        console.log("Is not a number");
     }
   process.exit();
 })
